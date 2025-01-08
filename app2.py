cf196a17e22f468621dba73b6bbc538190d04698
web-coder <yasher18@bk.ru>
2025-01-08 17:39:02 +0300

    делаю скрипты по срезам и индексам строки в питоне

--- app2.py
+++ app2.py
@@ -10,3 +10,38 @@ else:
 print(s1 == s2)
 print(s1 < s2)
 print(s1 > s2)
+
+
+#3.2 Индексы и срезы строк
+
+#Подвиг 1. Напишите программу ввода строки и отображения на экране
+#ее первого и последнего символа в виде одной строки.
+
+#Sample Input: I love Python
+#Sample Output: In
+
+text = input("input a text with spaces >>> ")
+textSym1 = text[0]
+textSymLast = text[len(text) - 1]
+
+print(textSym1, " ", textSymLast)
+
+
+#3.2 Индексы и срезы строк
+
+#Подвиг 4. Напишите программу отображения первых четырех символов
+#из введенной строки. Будем полагать, что строка гарантированно
+#длиной не менее четырех символов.
+
+#Sample Input: panda
+#Sample Output: pand
+
+checkStatus = True
+
+while checkStatus:
+    teta = input("input a word where her length > 4 symbols >>> ")
+    if len(teta) < 4:
+        checkStatus = True
+    else:
+        print(teta[0], teta[1], teta[2], teta[3])
+        checkStatus = False
